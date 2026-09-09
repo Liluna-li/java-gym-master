f10c94062b6298c0e5cfd7e4f92b5c176b1a3ec1
liluna <mmoonagirl@gmail.com>
2026-09-09 20:59:37 +0300

    fix review

--- test/ru/yandex/practicum/gym/TimetableTest.java
+++ test/ru/yandex/practicum/gym/TimetableTest.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.gym;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
